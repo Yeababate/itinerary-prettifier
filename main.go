4390a6e99bd96442040beadc7f122148d69dff00
Yeababate <yeababate1990@gmail.com>
2026-05-02 12:20:45 +0300

    Handle error and edge cases

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 import ("fmt" 
-"bufio" 
+// "bufio" 
 "os"
 "strings"
 "regexp"
 "time"
+"encoding/csv"
 )
 
 var Reset = "\033[0m"
 var Red = "\033[31m"
 var Green = "\033[32m"
 var Magenta = "\033[35m"
+var Italic = "\033[3m"
+var Bold = "\033[1m"
 
 type Data struct {
 	name string
@@ -25,23 +28,29 @@ func GetName(FileName string)[]Data{
 	file, err := os.Open(FileName)
 	if err != nil {
 		fmt.Println(Red + "Error opening file" + Reset)
-		os.Exit(1)
+		os.Exit(0)
 	}
-	defer file.Close()
+	reader:= csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(Red + Bold + "Airport lookup malformed" + Reset)
+		os.Exit(0)
+	}
 
 	DataStorage := []Data{}
-	EachData := Data{}
-	EachLine := ""
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan(){
-		EachLine = scanner.Text()
-		Column := strings.Split(EachLine, ",")
-		EachData.name = Column[0] 
-		EachData.iso_country = Column[1]
-		EachData.municipality = Column[2]
-		EachData.icao_code = Column[3]
-		EachData.iata_code = Column[4]
-		EachData.coordinates = Column[5]
+	for _, Column := range records {
+		if len(Column) != 6 {
+    		fmt.Println(Red + Bold + "Airport lookup malformed jsdh" + Reset)
+    		os.Exit(0)
+		}
+		EachData := Data{
+			name: Column[0],
+			iso_country: Column[1],
+			municipality: Column[2],
+			icao_code: Column[3],
+			iata_code: Column[4],
+			coordinates: Column[5],
+		}
 		DataStorage = append(DataStorage, EachData)
 	}
 	return DataStorage
@@ -66,7 +75,10 @@ func Malformed(Column Data) bool{
 	var data = []string{name, iso_country, municipality, icao_code, iata_code, coordinates}
 
 	for _, val := range data{
-		if strings.TrimSpace(val) == "" {               
+		if val == "" {
+			return true
+		}
+		if strings.ContainsRune(val, '�') {
 			return true
 		}
 	}
@@ -74,7 +86,7 @@ func Malformed(Column Data) bool{
 }
 
 func GetIATACode(FileName string, Csv string) string {
-	reIATA := regexp.MustCompile("#[A-Z]{3}")
+	reIATA := regexp.MustCompile(`#[A-Z]{3}\b`)
 	SingleRowIATA := reIATA.FindAllString(FileName,-1)
 	alldata := GetName(Csv)
 	for i := 0; i < len(SingleRowIATA); i++ {
@@ -92,14 +104,14 @@ func GetIATACode(FileName string, Csv string) string {
 }
 
 func GetICAOCode(FileName string, Csv string) string {
-	reICAO := regexp.MustCompile("##[A-Z]{4}")
+	reICAO := regexp.MustCompile(`##[A-Z]{4}\b`)
 	SingleRowICAO := reICAO.FindAllString(FileName,-1)
 	alldata := GetName(Csv)
 	for i := 0; i < len(SingleRowICAO); i++ {
 		for k := 0; k < len(alldata); k++ {
 			if(SingleRowICAO[i][2:] == alldata[k].icao_code){
 				if Malformed(alldata[k]) == true{
-				fmt.Println(Red + "Airport lookup malformed" + Reset)
+				fmt.Println(Red + Bold + "Airport lookup malformed" + Reset)
 				os.Exit(0)
 			}
 				FileName = strings.Replace(FileName, SingleRowICAO[i], alldata[k].name, 1)
@@ -110,13 +122,20 @@ func GetICAOCode(FileName string, Csv string) string {
 }
 
 func GetCityName(FileName string, Csv string) string {
-	reCity := regexp.MustCompile("\\*#[A-Z]{3}")
+	reCity := regexp.MustCompile(`\*#[A-Z]{3}\b|\*##[A-Z]{4}\b`)
 	SingleRowCity := reCity.FindAllString(FileName,-1)
 	alldata := GetName(Csv)
 	for i := 0; i < len(SingleRowCity); i++ {
 		for k := 0; k < len(alldata); k++ {
-			if(SingleRowCity[i][2:] == alldata[k].iata_code){
-				FileName = strings.Replace(FileName, SingleRowCity[i], alldata[k].municipality, 1)
+			if strings.HasPrefix(SingleRowCity[i], "*#"){
+				if(SingleRowCity[i][2:] == alldata[k].iata_code) {
+					FileName = strings.Replace(FileName, SingleRowCity[i], alldata[k].municipality, 1)
+				}
+			}
+			if strings.HasPrefix(SingleRowCity[i], "*##"){
+				if(SingleRowCity[i][3:] == alldata[k].icao_code) {
+					FileName = strings.Replace(FileName, SingleRowCity[i], alldata[k].municipality, 1)
+				}
 			}
 		}
 	}
@@ -124,7 +143,7 @@ func GetCityName(FileName string, Csv string) string {
 }
 
 func ReadDate (OutPut string) string{
-	reDate := regexp.MustCompile("D\\((.*?)\\)")
+	reDate := regexp.MustCompile(`D\((.*?)\)`)
 	Date := reDate.FindAllString(OutPut, -1)
 	for i := 0; i < len(Date); i ++ {
 		d, err := time.Parse("2006-01-02", Date[i][2:12])
@@ -138,9 +157,12 @@ func ReadDate (OutPut string) string{
 }
 
 func Read12hrTime (OutPut string) string{
-	reTime12 := regexp.MustCompile("T12\\((.*?)\\)")
+	reTime12 := regexp.MustCompile(`T12\((.*?)\)`)
 	Time12hr := reTime12.FindAllString(OutPut, -1)
 	for i := 0; i < len(Time12hr); i++ {
+		if len(Time12hr[i]) < 26 {
+			continue
+		}
 		t, err := time.Parse("2006-01-02T15:04-07:00", Time12hr[i][4:26])
 		if err != nil {
 			continue
@@ -155,6 +177,9 @@ func Read24hrTime (OutPut string) string{
 	reTime24 := regexp.MustCompile(`T24\((.*\d)\)`)
 	Time24hr := reTime24.FindAllString(OutPut, -1)
 	for i := 0; i < len(Time24hr); i++ {
+		if len(Time24hr[i]) < 26 {
+			continue
+		}
 		t, err := time.Parse("2006-01-02T15:04-07:00", Time24hr[i][4:26])
 		if err != nil {
 			continue
@@ -169,6 +194,9 @@ func Read12ZTime (OutPut string) string{
 	reTime12z := regexp.MustCompile("T12\\((.*[Zz])\\)")
 	Time12Z := reTime12z.FindAllString(OutPut, -1)
 	for i := 0; i < len(Time12Z); i++ {
+		if len(Time12Z[i]) < 21 {
+			continue
+		}
 		t, err := time.Parse("2006-01-02T15:04Z", Time12Z[i][4:21])
 		if err != nil {
 			continue
@@ -183,6 +211,9 @@ func Read24ZTime (OutPut string) string{
 	reTime24z := regexp.MustCompile("T24\\((.*[Zz])\\)")
 	Time24Z := reTime24z.FindAllString(OutPut, -1)
 	for i := 0; i < len(Time24Z); i++ {
+		if len(Time24Z[i]) < 21 {
+			continue
+		}
 		t, err := time.Parse("2006-01-02T15:04Z", Time24Z[i][4:21])
 		if err != nil {
 			continue
@@ -194,7 +225,7 @@ func Read24ZTime (OutPut string) string{
 }
 
 func VerticalSpaces (output string) string {
-	reVertical:= regexp.MustCompile(`\v|\f|\r`)
+	reVertical:= regexp.MustCompile(`\v|\f|\r\n`)
 	output = reVertical.ReplaceAllString(output, "\n")
 
 	reLimit := regexp.MustCompile(`\n{3,}`)
@@ -213,29 +244,25 @@ func WriteToOutput(name string, output string){
 func CheckExists(input string, csv string) bool{
 	_,err := os.Stat(input)
 	if os.IsNotExist(err){
-		fmt.Println(Red + "Input not found." + Reset)
+		fmt.Println(Red + Bold + "Input not found." + Reset)
 		return false
 	}
 	_, err = os.Stat(csv)
 	if os.IsNotExist(err){
-		fmt.Println(Red + "Airport lookup not found." + Reset)
+		fmt.Println(Red + Bold + "Airport lookup not found." + Reset)
 		return false
 	}
 	return true
 }
 
 func main(){
-	if len(os.Args) != 4{
-		if(os.Args[1] == "-h" || os.Args[1] == "-H") {
-			fmt.Println(Green + "itinerary usage:\ngo run . ./input.txt ./output.txt ./airport-lookup.csv" + Reset)
-			return
-		}
+	if len(os.Args) != 4 || os.Args[1] == "-h" || os.Args[1] == "-H" {
 		fmt.Println(Green + "itinerary usage:\ngo run . ./input.txt ./output.txt ./airport-lookup.csv" + Reset)
-		return
+		os.Exit(0)
 	}
 
 	if CheckExists(os.Args[1],os.Args[3]) == false {
-		return
+		os.Exit(0)
 	}
 
 	input := os.Args[1]
@@ -252,5 +279,5 @@ func main(){
 	OutPutMessage = Read12hrTime(OutPutMessage)
 	OutPutMessage = Read24hrTime(OutPutMessage)
 	WriteToOutput(output, OutPutMessage)
-	fmt.Println(Magenta + OutPutMessage + Reset)
+	fmt.Println(Magenta + Italic + OutPutMessage + Reset)
 }
